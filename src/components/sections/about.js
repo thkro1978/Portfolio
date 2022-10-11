f428f1dc83b97dc98346d3566103152485fe5335
thkro1978 <114503531+thkro1978@users.noreply.github.com>
2022-10-11 20:00:38 +0100

    Update about.js

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -61,7 +61,6 @@ const StyledPic = styled.div`
     position: relative;
     width: 100%;
     border-radius: var(--border-radius);
-    background-color: var(--green);
 
     &:hover,
     &:focus {
@@ -81,9 +80,6 @@ const StyledPic = styled.div`
     .img {
       position: relative;
       border-radius: var(--border-radius);
-      mix-blend-mode: multiply;
-      filter: grayscale(100%) contrast(1);
-      transition: var(--transition);
     }
 
     &:before,
@@ -125,7 +121,7 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
+  const skills = ['Human-Centered Design', 'User Research', 'Product Thinking', 'Interaction Design', 'UX Auditing & QA', 'UX Leadership', 'Figma' , 'Miro' , 'Confluence' , 'Jira'];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
@@ -135,33 +131,22 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              Hello! My name is Thomas and I enjoy creating interactive experiences that improve our lives. My interest in design started back in 1999 when I got my
+              first Mac computer, creating interactive animations and learning the foundations of good design.
             </p>
 
             <p>
-              Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
+              Fast-forward to today, I no have 20 years design experience and have the privilege of working at{' '}
+              <a href="https://oracle.com/">a rather large tech company</a>. Previously I worked at {' '}
+              <a href="https://appadvice.com/app/autotrip-mileage-tracker/1480711539"> a global invoice automation company</a>,{' '}
+              <a href="https://www.tungsten-network.com">a start-up</a>, and{' '}
+              <a href="https://kroell.co.uk/">my own digital design studio</a>. My
               main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
+              experiences at <a href="https://oracle.com/">Oracle</a> for a variety of
+              clients in the EMEA region.
             </p>
 
-            <p>
-              I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
-              </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
-            </p>
-
-            <p>Here are a few technologies I’ve been working with recently:</p>
+            <p>Here are a few of my skills and technologies I'm using:</p>
           </div>
 
           <ul className="skills-list">
