aef189d290708c668ad33c2444a4f24ebfe53960
thkro1978 <114503531+thkro1978@users.noreply.github.com>
2022-10-11 19:54:02 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Thomas Kroell',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      '',
+    siteUrl: 'https://delightful-fudge-3f5005.netlify.app/', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@kingkroell',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
